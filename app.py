31e9fb9ca26c400c8675092c963e2cae232dce88
VedeshP <138141742+VedeshP@users.noreply.github.com>
2024-04-13 05:39:24 +0000

    boilerplate2 in app

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from flask import Flask, flash, redirect, render_template, request, session, jso
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+import datetime
+
+app = Flask(__name__)
 
