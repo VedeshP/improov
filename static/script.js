bc9d5851b9ef69d5cf860a665195813d0c888fb9
VedeshP <138141742+VedeshP@users.noreply.github.com>
2024-06-02 04:50:44 +0000

    static update

--- static/script.js
+++ static/script.js
@@ -19,3 +19,20 @@ const switchTheme = ( ) => {
 document.querySelectorAll('#theme-switcher').forEach(switcher => {
     switcher.addEventListener('click', switchTheme);
 });
+
+let lastScrollTop = 0;
+        const navbar = document.querySelector('.custom-navbar-bottom');
+    
+        window.addEventListener('scroll', function() {
+            const currentScroll = window.scrollY || document.documentElement.scrollTop;
+    
+            if (currentScroll > lastScrollTop) {
+                // Scrolling down
+                navbar.classList.add('hidden');
+            } else {
+                // Scrolling up
+                navbar.classList.remove('hidden');
+            }
+    
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+        });
