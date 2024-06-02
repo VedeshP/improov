f70b19fce830eb6ef806ca506c4884d3ea32f739
VedeshP <138141742+VedeshP@users.noreply.github.com>
2024-06-02 13:27:00 +0000

    backend started

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from flask import Flask, flash, redirect, render_template, request, session, jso
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from pyembed.core import PyEmbed
+from sqlalchemy.exc import IntegrityError
 
 import datetime
 
-from helpers import login_required
+from helpers import login_required, apology, check_password_strength_basic
 
 app = Flask(__name__)
 pyembed_instance = PyEmbed()
@@ -56,8 +57,41 @@ def know_more():
 
 @app.route("/login" , methods = ["GET","POST"])
 def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
     if request.method == "POST":
-        ...
+        # Ensure username was submitted
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username:
+            return apology("Must provide username", 403)
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("Must provide password", 403)
+        
+        # Query database for username
+        rows = db.session.execute(
+            text(
+                "SELECT * FROM users WHERE username = :username"
+            ), {"username" : username}
+        ).fetchall
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0][2], request.form.get("password") 
+        ):
+            return apology("invalid username and/or password", 403)
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+
+        # Redirect user to home page
+        return redirect("/")
+
     else:
         return render_template("login.html", show_taskbar = False)
 
@@ -71,8 +105,67 @@ def logout():
 
 @app.route("/signup", methods = ["GET", "POST"])
 def signup():
+    """Register user"""
+    # Forget any user_id
+    session.clear()
+
     if request.method == "POST":
-        ...
+        
+        username = request.form.get("username")
+        username = username.strip().lower()
+        display_name = request.form.get("display_name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+        birthday = request.form.get("birthday")
+
+        for i in username:
+            if i == ' ':
+                return apology("username must not contain space", 400)
+        if not username:
+            return apology("Must provide username", 400)
+        if not display_name:
+            return apology("Must provide Display Name", 400)
+        if not email:
+            return apology("Please provide Email Id", 400)
+        if not password:
+            return apology("Please set a password", 400)
+        if not confirm_password:
+            return apology("Must confirm password", 400)
+        if password != confirm_password:
+            return apology("Both password must be same", 403)
+        if not birthday:
+            return apology("Please provide birthdate", 400)
+        
+        if check_password_strength_basic(password):
+            return apology("Password must contain atleast 8 characters, a special character, letters and numbers", 403)
+        
+        hash = generate_password_hash(password)
+
+        try:
+            # Add user detials to the database
+            # In place of password only hash is stored for safety and privacy
+            db.session.execute(
+                text(
+                    "INSERT INTO users (username, password, display_name, date_of_birth, email_id) VALUES( :username, :hash, :display_name, :date_of_birth, :email_id)"
+                ), {"username" : username, "hash" : hash, "display_name": display_name, "date_of_birth" : birthday, "email_id" : email}
+            )
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
+            if "UNIQUE constraint failed: users.username" in str(e):
+                return apology("Username already exists")
+            elif "UNIQUE constraint failed: users.email_id" in str(e):
+                return apology("Email ID already exists")
+            else:
+                return apology("An integrity error occurred")
+        except Exception as e:
+            db.session.rollback()
+            return apology("An unexpected error occurred: " + str(e))
+        
+
+        flash("Signed Up! Login to proceed")
+        return redirect("/login")
     else:
         return render_template("signup.html", show_taskbar = False)
 
@@ -80,10 +173,31 @@ def signup():
 @app.route("/post", methods = ["GET", "POST"])
 # @login_required
 def post():
+    """Let user post"""
+
+    user_id = session["user_id"]
+
+    rows = db.session.execute(
+        text(
+            "SELECT * FROM users WHERE id = :user_id"
+        ), {"user_id" : user_id}
+    ).fetchall()
+
+    username = rows[0][1]
+    display_name = rows[0][3]
+
     if request.method == "POST":
-        ...
+        primary_topic = request.form.get("primary_topic")
+        internal_topic = request.form.get("internal_topic")
+        post_content = request.form.get("post_content")
+        link = request.form.get("link")
+
+        #TODO: INSERT INTO the table with the date and time of posting the thing use datetime
+        
+        return redirect("/")
+    
     else:
-        return render_template("post.html", active_page = 'post', show_taskbar= True)
+        return render_template("post.html", active_page = 'post', show_taskbar= True, username=username, display_name=display_name)
 
 
 @app.route("/communities")
--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 from flask import redirect, render_template, request, session
 from functools import wraps
+import re
+
 
 def login_required(f):
     """
@@ -15,3 +17,49 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def check_password_strength_basic(password):
+    if not re.search(r'[A-Za-z]', password):
+        return True
+    if not re.search(r'[0-9]', password):
+        return True
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        return True
+    return False
+
+# old code
+# def check_password_strength_basic(password):
+#         special_characters = '!@#$%^&*()-+?_=,<>/"'
+#         if len(password) < 8:
+#             return True
+#         elif not any(c in special_characters for c in password):
+#             return True
+#         elif not any(c.isalnum() for c in password):
+#             return True
+#         return False
